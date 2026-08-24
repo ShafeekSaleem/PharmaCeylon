0af8456acee4597e566511d394cdaac610406262
Shafeek Saleem <47132399+ShafeekSaleem@users.noreply.github.com>
2026-08-25 02:03:51 +0530

    security: register RLS startup validator

--- apps/api/src/prisma/prisma.module.ts
+++ apps/api/src/prisma/prisma.module.ts
@@ -1,10 +1,15 @@
 import { Global, Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
+import { RlsCanaryStartupValidator } from "./rls-canary-startup.validator";
 import { TenantTransactionContext } from "./tenant-transaction-context.service";
 
 @Global()
 @Module({
-  providers: [PrismaService, TenantTransactionContext],
+  providers: [
+    PrismaService,
+    TenantTransactionContext,
+    RlsCanaryStartupValidator,
+  ],
   exports: [PrismaService, TenantTransactionContext],
 })
 export class PrismaModule {}
